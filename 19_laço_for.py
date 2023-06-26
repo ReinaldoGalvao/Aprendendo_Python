8e7b6fea3c4e430f21f3c49de93fc6861c7c6647
MAIQUE100 <99631783+ReinaldoGalvao@users.noreply.github.com>
2023-06-26 09:06:01 -0300

    Update 19_laço_for.py

--- 19_laço_for.py
+++ 19_laço_for.py
@@ -1,3 +1,10 @@
-for i in range(100, 0, -1):
+'''for i in range(100, 0, -1):
     print(i)
-    
+'''
+
+
+for i in range (100, 1, -1):
+    if i % 2 == 0:
+        print(i)
+        
+print(list(range(0, 51, 2)))
