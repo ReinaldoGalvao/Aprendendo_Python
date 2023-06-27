914c1cbaca15c0c96548331ffc715a1af3aca94e
MAIQUE100 <99631783+ReinaldoGalvao@users.noreply.github.com>
2023-06-27 15:49:36 -0300

    exercicios

---
+++ EX/01exercicios_while.py
@@ -0,0 +1,23 @@
+#Faça um programa que peça uma nota, entre zero e dez
+#Mostre uma mensagem caso o valor seja inválido e continue pedindo até que o usuário informe um valor válido.
+
+
+while True:
+    nota = int(input('Digite uma nota entre 0 e 10: '))
+    
+    if nota < 0 or nota > 10:
+        print('ta de sacanagem')
+        continue
+    else:
+        print('acertou, mizeravi')
+        """break"""
+
+    
+    
+""""    
+while nota < 0 or nota > 10:
+    nota = int(input('Ditite uma nota entre 0 e 10: '))
+    if nota < 0 or nota > 10:
+        print('Ta d+ né cara')
+print('Acertou!!!!!!!!!!!!!!!!')
+print(f'Parabens, você digitou a nota {nota}')"""
--- laço_de_repetiçao.py
+++ laço_de_repetiçao.py
@@ -1,4 +1,14 @@
+"""
 i = 0
 while i < 5:
     print(f'oi o i vale {i}')
+    i += 1
+"""
+
+i = 0
+while i < 10:
+    print(i)
+    if i % 2 == 1:
+        i = i + 2
+        continue
     i += 1
