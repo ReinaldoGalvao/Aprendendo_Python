51a26e236edffd741ca665709bba4d86d841d708
MAIQUE100 <99631783+ReinaldoGalvao@users.noreply.github.com>
2023-06-28 09:49:22 -0300

    ex

---
+++ 04_while.py
@@ -0,0 +1,17 @@
+"""
+Pais A 80000
+    3% ano
+
+Pais B 200000    
+    1.5% ano
+    
+    """
+    
+a = 80000
+b = 200000
+anos = 0
+while a < b:
+    a * (3/100)
+    anos += 1
+    
+print(anos)
