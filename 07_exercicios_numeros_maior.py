897fd91f1c2ce37c85ff4ba01555bf81feafe0bf
MAIQUE100 <99631783+ReinaldoGalvao@users.noreply.github.com>
2023-06-28 11:46:12 -0300

    ex

--- 07_exercicios_numeros_maior.py
+++ 07_exercicios_numeros_maior.py
@@ -1,13 +1,21 @@
 # Inicializa a variável do maior número com um valor inicial baixo
 maior_numero = float('-inf')
+soma = 0
 
-# Loop para ler os números
+# Loop para ler os números e calcular a soma
 for i in range(5):
     numero = float(input("Digite um número: "))
     
     # Verifica se o número lido é maior que o maior número atual
     if numero > maior_numero:
         maior_numero = numero
+    
+    soma += numero
 
-# Após ler todos os números, exibe o maior número encontrado
+# Calcula a média dos números
+media = soma / 5
+
+# Exibe o maior número, a soma e a média
 print("O maior número é:", maior_numero)
+print("A soma dos números é:", soma)
+print("A média dos números é:", media)
