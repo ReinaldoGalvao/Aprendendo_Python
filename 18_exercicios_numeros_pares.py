a20dbfacee1bbd181c772d71bb449b731a41609e
KingNaldo <99631783+ReinaldoGalvao@users.noreply.github.com>
2023-07-17 15:10:55 -0300

    Update 18_exercicios_numeros_pares.py

--- 18_exercicios_numeros_pares.py
+++ 18_exercicios_numeros_pares.py
@@ -1,8 +1,8 @@
 #Receba um numero e mostre todos os numeros pares de 0 ate o numero digitado.
-n1 = int(input('Digite um numero: '))
+n2 = int(input('Digite um numero: '))
 
 i = 1
-while i <= n1:
+while i <= n2:
     if i % 2 == 0:
         print(i)
     i += 1
