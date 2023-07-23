1659b4a1ea735ed0782fa39fb0797f99f80e0dfb
KingNaldo <99631783+ReinaldoGalvao@users.noreply.github.com>
2023-07-23 03:21:35 -0300

    Create velocidade_down.py

---
+++ velocidade_down.py
@@ -0,0 +1,15 @@
+'''
+Faça um programa que peça o tamanho de um arquivo para download (em MB)
+e a velocidade de um link de Internet (em Mbps)
+calcule e informe o tempo aproximado de download do arquivo usando este link (em minutos). 
+'''
+tamanho = int(input('Diga o tamanho do arquivo em MB: '))
+velocidade = int(input('Diga qual a velocidade da sua internet em Mbps: '))
+
+taxa_dow = velocidade * 0.119209
+minuto_mbs = taxa_dow * 60
+hora_mbs = minuto_mbs * 60
+temp_dow = tamanho / minuto_mbs
+
+print('O download ira demorar {:.2f} minutos'.format(temp_dow))
+print(minuto_mbs)
