8261a3294ae1bc3234dfe35e18ef1dfe21222c54
MAIQUE100 <99631783+ReinaldoGalvao@users.noreply.github.com>
2023-07-24 17:13:56 -0300

    Update criando_def.py

--- criando_def.py
+++ criando_def.py
@@ -1,6 +1,34 @@
-def boas_vidas(nome, qunatidade):
+'''
+def boas_vidas(quantidade, nome='Linda'):
     print(f'Olá {nome}.')
-    print(f'Temos {str(qunatidade)} laptops em estoque')
+    print(f'Temos {str(quantidade)} laptops em estoque')
     
-boas_vidas('Marcos')
+boas_vidas(6)
 
+//////////////////////////////
+
+def cliente1(nome):
+    print(f'Olá {nome}')
+    
+
+def cliente2(nome):
+    return f'Olá {nome}'
+    
+
+
+x= cliente1('Maria')
+y = cliente2('Jose')
+print(x)
+print(y)
+//////////////////////////////
+
+'''
+
+def soma(*numeros):
+    resultado = 0
+    for nun in numeros:
+        resultado += nun 
+    return resultado
+    
+x = soma(2, 3, 4, 7)
+print(x)
