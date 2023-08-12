b93f72cd0e340e08c1316f6e942cc3f6495049aa
KingNaldo <99631783+ReinaldoGalvao@users.noreply.github.com>
2023-08-12 01:31:16 -0300

    Create tupla.py

---
+++ tupla.py
@@ -0,0 +1,21 @@
+'''lanche = ('Hamburguer', 'Suco', 'Pizza', 'Pudim', 'Batata Frita')
+print(lanche)
+
+for comida in lanche:
+    print(f'Eu vou comer {comida}')
+
+for cont in range(0, len(lanche)):
+    print(f'Eo vou comer {lanche[cont]} na posição {cont}')
+
+for pos, comida in enumerate(lanche):
+    print(f'Eu vou comer {comida} na posição {pos}')
+
+print('Comi pra caramba!')
+
+
+a = (2, 5, 4)
+b = (5, 8, 1, 2)
+c = b + a
+print(c.count(5))'''
+
+
