cfbcbe55b5aeb41192e1e4158dc19032086f1d5f
MAIQUE100 <99631783+ReinaldoGalvao@users.noreply.github.com>
2023-09-12 09:54:25 -0300

    Create Ex_tuplas.py

---
+++ Ex_tuplas.py
@@ -0,0 +1,12 @@
+'''
+Faça um Programa que leia um vetor de 5 números inteiros e mostre-os.
+'''
+
+numeros = (int(input('Diga um numero: ')),
+            int(input('Diga outro numero: ')),
+            int(input('Diga mais um numero: ')),
+            int(input('Fale um numero: ')),
+            int(input('Diga mais uma vez numero: ')),
+            int(input('Diga um numero novamente: ')),)
+
+print(numeros)
