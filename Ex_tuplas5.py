9adb8ae53c26b1ddffe8faafb25b3c68f8e778ff
MAIQUE100 <99631783+ReinaldoGalvao@users.noreply.github.com>
2023-09-12 16:02:08 -0300

    Create Ex_tuplas5.py

---
+++ Ex_tuplas5.py
@@ -0,0 +1,34 @@
+'''
+Faça um Programa que leia 20 números inteiros e armazene-os num vetor.
+Armazene os números pares no vetor PAR e os números IMPARES no vetor impar.
+Imprima os três vetores.
+'''
+
+numeros = (
+    (int(input('Diga um numero: '))),
+    (int(input('Diga um numero: '))),
+    (int(input('Diga um numero: '))),
+    (int(input('Diga um numero: '))),
+    (int(input('Diga um numero: '))),
+    (int(input('Diga um numero: '))),
+    (int(input('Diga um numero: '))),
+    (int(input('Diga um numero: '))),
+    (int(input('Diga um numero: '))),
+    (int(input('Diga um numero: '))),
+    (int(input('Diga um numero: '))),
+    (int(input('Diga um numero: '))),
+    (int(input('Diga um numero: '))),
+    (int(input('Diga um numero: '))),
+    (int(input('Diga um numero: '))),
+    (int(input('Diga um numero: '))),
+    )
+par = []
+impar = []
+for i in numeros:
+    if i % 2 != 0:
+        impar.append(i)
+    else:
+        par.append(i)
+print(f'Os números digitados foram {numeros}')
+print(f'Os números pares digitados são: {" ".join(map(str, par))}')
+print(f'Os números ímpares digitados são: {" ".join(map(str, impar))}')
