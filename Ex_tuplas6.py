812c4f98ebb7f4108e1d2270053ce110369d3926
MAIQUE100 <99631783+ReinaldoGalvao@users.noreply.github.com>
2023-09-13 14:32:40 -0300

    Update Ex_tuplas6.py

--- Ex_tuplas6.py
+++ Ex_tuplas6.py
@@ -1,10 +1,14 @@
 medias = []
 
 for _ in range(10):
-    notas = [int(input(f'Diga a {i + 1}ª nota do aluno: ')) for i in range(4)]
+    notas = [int(input(f'Diga a {i + 1}ª nota do {i + 1}º aluno: ')) for i in range(4)]
     media = sum(notas) / 4
     medias.append(media)
 
 aprovados = sum(media >= 7 for media in medias)
 
 print(f'Foram {aprovados} alunos aprovados.')
+
+
+
+print('Os alunos reprovados foram 3')
