1739b54bd4c5c4320b4672076f89b6296f662626
KingNaldo <99631783+ReinaldoGalvao@users.noreply.github.com>
2023-10-15 15:20:36 -0300

    Update exetuplasfor.py

--- exetuplasfor.py
+++ exetuplasfor.py
@@ -1,7 +1,13 @@
 lanche = ('Hamburguer', 'Suco', 'Pizza', 'Pudim')
 
-for comida in lanche:
-    if comida == 'Suco':
-        print(f'Eu vou beber {comida}!')
-    print(f'Eu vou comer {comida}!')
+for cont in range(0, len(lanche)):
+    if lanche[cont] == 'Suco':
+        print(f'Eu vou beber {lanche[cont]}!')
+    print(f'Eu vou comer {lanche[cont]}')
+
+
+# for comida in lanche:
+#     if comida == 'Suco':
+#         print(f'Eu vou beber {comida}!')
+#     print(f'Eu vou comer {comida}!')
 print(f'Comi pra caramba!')
