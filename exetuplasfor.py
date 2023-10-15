bf95660ef8d48d445f1fe1902eb28052186de51f
KingNaldo <99631783+ReinaldoGalvao@users.noreply.github.com>
2023-10-15 15:16:10 -0300

    Create exetuplasfor.py

---
+++ exetuplasfor.py
@@ -0,0 +1,7 @@
+lanche = ('Hamburguer', 'Suco', 'Pizza', 'Pudim')
+
+for comida in lanche:
+    if comida == 'Suco':
+        print(f'Eu vou beber {comida}!')
+    print(f'Eu vou comer {comida}!')
+print(f'Comi pra caramba!')
