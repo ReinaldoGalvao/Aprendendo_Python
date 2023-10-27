ffd9755665f7ba92d5ca4be4e7bc431270c3fee1
KingNaldo <99631783+ReinaldoGalvao@users.noreply.github.com>
2023-10-26 22:07:12 -0300

    Create main.py

---
+++ insta_auto/main.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
+import pyautogui
+
+from selenium import webdriver
+
+# Substitua 'caminho_para_chromedriver' pelo caminho real para o chromedriver.exe no seu sistema
+chrome_driver_path = 'caminho_para_chromedriver'
+driver = webdriver.Chrome(executable_path=chrome_driver_path)
+driver.get('http://instagram.com')
+driver.maximize_window()
+
+driver = webdriver.Chrome()
+driver.get('http://instagram.com')
+driver.maximize_window()
+time.sleep(4)
+
+
+time.sleep(2000)
