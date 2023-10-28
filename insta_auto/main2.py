f84a4f3d2e94ef61da331829373989a808b02164
KingNaldo <99631783+ReinaldoGalvao@users.noreply.github.com>
2023-10-28 02:45:07 -0300

    Update main2.py

--- insta_auto/main2.py
+++ insta_auto/main2.py
@@ -6,7 +6,7 @@ with sync_playwright() as p:
     page = context.new_page()
 
     page.goto('http://instagram.com')
-    page.set_viewport_size({"width": 1920, "height": 1080})
+    page.set_viewport_size({"width": 1366, "height": 768})
 
     page.wait_for_timeout(4000)  
     page.wait_for_timeout(2000)
@@ -15,14 +15,21 @@ with sync_playwright() as p:
     page.click('xpath=/html/body/div[2]/div/div/div[2]/div/div/div/div[1]/section/main/article/div[2]/div[1]/div[2]/form/div/div[1]/div/label/input')
 
     # Digitando texto no campo
-    page.type('xpath=/html/body/div[2]/div/div/div[2]/div/div/div/div[1]/section/main/article/div[2]/div[1]/div[2]/form/div/div[1]/div/label/input', 'fabioz')
+    page.type('xpath=/html/body/div[2]/div/div/div[2]/div/div/div/div[1]/section/main/article/div[2]/div[1]/div[2]/form/div/div[1]/div/label/input', 'crovis@mail.com')
     page.wait_for_timeout(2000)
     page.click('xpath=/html/body/div[2]/div/div/div[2]/div/div/div/div[1]/section/main/article/div[2]/div[1]/div[2]/form/div/div[2]/div/label/input')
     page.wait_for_timeout(2000)
     
-    page.type('xpath=/html/body/div[2]/div/div/div[2]/div/div/div/div[1]/section/main/article/div[2]/div[1]/div[2]/form/div/div[2]/div/label/input', '1111111')
+    page.type('xpath=/html/body/div[2]/div/div/div[2]/div/div/div/div[1]/section/main/article/div[2]/div[1]/div[2]/form/div/div[2]/div/label/input', '796410Qq**')
     page.wait_for_timeout(1500)
     page.click('xpath=/html/body/div[2]/div/div/div[2]/div/div/div/div[1]/section/main/article/div[2]/div[1]/div[2]/form/div/div[3]/button/div')
+
+    page.wait_for_timeout(1357)
+    page.click('xpath=/html/body/div[2]/div/div/div[2]/div/div/div/div[1]/div[1]/div[2]/section/main/div/div/div/div/div')
+
+    page.wait_for_timeout(1557)
+    page.click('xpath=/html/body/div[3]/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[3]/button[2]')
+    
     
     
     page.wait_for_timeout(2000)
