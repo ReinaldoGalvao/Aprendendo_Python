f71ae62e442dcc757a4fbc32887392fefdb0e608
MAIQUE100 <99631783+ReinaldoGalvao@users.noreply.github.com>
2023-11-10 18:53:25 -0300

    Update criando_def.py

--- criando_def.py
+++ criando_def.py
@@ -24,11 +24,43 @@ print(y)
 
 '''
 
-def soma(*numeros):
-    resultado = 0
-    for nun in numeros:
-        resultado += nun 
-    return resultado
+# def soma(*numeros):
+#     resultado = 0
+#     for nun in numeros:
+#         resultado += nun 
+#     return resultado
+
+def somar(lista): #criei essa função para somar a lista numero
+    return sum(lista) #aqui retorna a soma da lista
+
+# x = soma(2, 3, 4, 7)
+# print(x)
+
+
+def calcular_media(lista):
+    if len(lista) == 0:
+        return "A lista esta vazia, não é possivel calcular a média."
     
-x = soma(2, 3, 4, 7)
+    soma = sum(lista)
+    
+    media = soma / len(lista)
+    
+    return media
+
+numeros = (int(input('Diga um numero: ')),
+            int(input('Diga outro numero: ')),
+            int(input('Diga mais um numero: ')),
+            int(input('Fale um numero: ')),
+            int(input('Diga mais uma vez numero: ')),
+            int(input('Diga mais uma vez numero: ')),
+            int(input('Diga mais uma vez numero: ')),
+            int(input('Diga um numero novamente: ')),)
+print(numeros)
+x = somar(numeros)
+
+mediax = calcular_media(numeros)
+
+
+
 print(x)
+print(mediax)
